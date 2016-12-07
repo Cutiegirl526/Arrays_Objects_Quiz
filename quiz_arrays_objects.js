16a4f4dd2b4fcef384fce1cd940c3ad80fb01e3e
cutiegirl526 <kayladiniega@gmail.com>
2016-12-07 08:31:42 -1000

    Failed my test... Brain fart and ect... Out of it!

---
+++ quiz_arrays_objects.js
@@ -0,0 +1,24 @@
+/*
+Kayla Diniega
+Period 1
+December 7, 2016
+Quiz: Arrays and Objects
+*/
+
+//array with two strings #1
+
+var myArray = ["10", "true"];
+
+//access the array and console log the number 10 #2a
+
+//function with one parameter #3
+
+function (true){
+	console.log();
+} else {
+	console.log("You Shall Not Pass!");
+}
+
+// Create var #4
+
+var batman
